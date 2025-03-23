1ecf88ce42774395f172eb0700fa9f80f1247a8d
Fernando Orellana <fernandoarieldb95@gmail.com>
2025-03-23 20:41:22 -0300

    modifique auto en el taller

--- resources/views/admin/workshop/index.blade.php
+++ resources/views/admin/workshop/index.blade.php
@@ -9,17 +9,12 @@
             <div class="flex flex-col mx-auto max-w-6xl">
                 <h1 class="text-center text-3xl font-bold mb-4 text-gray-200">EN EL TALLER</h1>
                 <section>
-                    <div class="grid md:grid-cols-2 grid-rows-2 gap-2  p-2 min-h-[300px] bg-back text-white">
-                        <div class=" rounded-sm p-2 bg-slate-500">
-                            <h4 class="font-bold mb-4">Descripción del problema:</h4>
-                            <article>
-                                <p>{{ $assigned_employee->vehicleInWorkshop->description }}</p>
-                            </article>
-                        </div>
-                        <div class="grid grid-cols-2 rounded-sm gap-2 p-2 row-span-2 bg-slate-500">
+                    <div class="grid md:grid-cols-2 grid-rows-2 gap-2  p-2 min-h-[300px] bg-back text-gray-900">
+                        <div class="grid grid-cols-2 rounded-md gap-2 p-2 row-span-1 bg-white ">
+                            {{-- Detalles del cliente --}}
                             <section>
                                 <h4 class="font-bold mb-4">Detalles del cliente:</h4>
-                                <article class="grid grid-cols-1 md:grid-cols-2">
+                                <article class="grid grid-cols-1 md:grid-cols-2 gap-3">
                                     <label class=" font-bold" for="">Nombre:</label>
                                     <p>{{ $assigned_employee->vehicleInWorkshop->name }}</p>
                                     <label class=" font-bold" for="">Correo:</label>
@@ -28,9 +23,10 @@
                                     <p>{{ $assigned_employee->vehicleInWorkshop->phone_number }}</p>
                                 </article>
                             </section>
+                            {{-- Detalles del vehiculo --}}
                             <section>
                                 <h4 class="font-bold mb-4">Detalles del vehiculo:</h4>
-                                <article class="grid grid-cols-1 md:grid-cols-2">
+                                <article class="grid grid-cols-1 md:grid-cols-2 gap-3">
                                     <label class=" font-bold" for="">Vehiclulo:</label>
                                     <p>{{ $assigned_employee->vehicleInWorkshop->vehicle_type }}</p>
                                     <label class=" font-bold" for="">Marca:</label>
@@ -43,14 +39,21 @@
                                 </article>
                             </section>
                         </div>
-                        <div class=" rounded-sm p-2 bg-slate-500">
+                        <div class=" rounded-md p-2 bg-white  row-start-2">
+                            <h4 class="font-bold mb-4">Descripción del problema:</h4>
+                            <article>
+                                <p>{{ $assigned_employee->vehicleInWorkshop->description }}</p>
+                            </article>
+                        </div>
+                        <div class=" rounded-md p-2 bg-white  row-span-2">
                             <h4 class="font-bold mb-4">Detalles del presupuesto actual:</h4>
                             <article>
 
                             </article>
                         </div>
+                        
                     </div>
-                </section>
+              {{--   </section>
                 <section>
                     <div class="grid grid-rows-1 gap-2 p-2 min-h-[300px] bg-back text-white">
                         <section>
@@ -74,7 +77,7 @@
                             </div>
                         </section>
                     </div>
-                </section>
+                </section> --}}
             </div>
         </div>
 </x-app-layout>
